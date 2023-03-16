ad59edd2100bc92d9ecc7549a3f126e8f3f12060
Ediversion <born4ice@hotmail.com>
2023-03-16 12:59:12 +0000

    sh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - This had better work
+ *
+ * Return: okay
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle,");
+puts("\"Programming is like building a multilingual puzzlei);
 return (0); }
