b20f4984051eabbe984a7983d6c7bb89008b0ec6
Ediversion <born4ice@hotmail.com>
2023-03-17 10:33:52 +0000

    last

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+* main - This had better work
+*
+* Return: okay
+*/
+
+int main(void)
+{
+int i, j;
+for (i = 0; i < 100; i++)
+{
+for (j = i; j < 100; j++)
+{
+if (i == j)
+{
+continue;
+}
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+putchar(' ');
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i != 99 || j != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
