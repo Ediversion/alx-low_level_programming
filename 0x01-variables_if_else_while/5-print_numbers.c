346246c9192eeee56d49d0cc651fcd6c4b45911f
Ediversion <born4ice@hotmail.com>
2023-03-17 09:39:56 +0000

    five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - This had better work
+*
+* Return: okay
+*/
+
+int main(void) {
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return 0;
+}
