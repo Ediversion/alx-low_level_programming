794df24e5a3932a92c0148bc61b7c4d29feecdeb
Ediversion <born4ice@hotmail.com>
2023-03-17 10:43:52 +0000

    last

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,24 +7,25 @@
 
 int main(void)
 {
-int i, j;
-for (i = 0; i < 100; i++)
+int i, j, k;
+
+for (i = 0; i < 10; i++)
 {
-for (j = i; j < 100; j++)
+for (j = 1; j < 10; j++)
 {
-if (i == j)
+for (k = 2; k < 10; k++)
 {
-continue;
-}
-putchar(i / 10 + '0');
-putchar(i % 10 + '0');
-putchar(' ');
-putchar(j / 10 + '0');
-putchar(j % 10 + '0');
-if (i != 99 || j != 98)
+if (i < j && j < k)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i + j + k != 24)
 {
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 }
