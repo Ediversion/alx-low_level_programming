ce4987a30539e2613c068e834355fad61c033f25
Ediversion <born4ice@hotmail.com>
2023-03-17 10:59:52 +0000

    lastlast

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,26 +7,20 @@
 
 int main(void)
 {
-int i, j, k;
-
-for (i = 0; i < 10; i++)
+int i, j;
+for (i = 0; i <= 98; i++)
 {
-for (j = 1; j < 10; j++)
+for (j = i + 1; j <= 99; j++)
 {
-for (k = 2; k < 10; k++)
-{
-if (i < j && j < k)
-{
-putchar(i + '0');
-putchar(j + '0');
-putchar(k + '0');
-if (i + j + k != 24)
-{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i == 98 && j == 99)
+continue;
 putchar(',');
 putchar(' ');
-}
-}
-}
 }
 }
 putchar('\n');
