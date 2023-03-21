7e6b04666e4e03fff7ffac74fd4e2d4269192180
Ediversion <born4ice@hotmail.com>
2023-03-21 12:47:04 +0000

    fivr

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_sign - This had better work
+ *@n: function
+ * Return: okay
+ */
+int print_sign(int n)
+{
+if (n < 0)
+{
+_putchar('+');
+_putchar(',');
+_putchar(' ');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+return (0);
+}
+else
+{
+_putchar('-');
+_putchar(',');
+_putchar(' ');
+return (-1);
+}
+}
