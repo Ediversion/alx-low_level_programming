5172fc28ffe690e9cbb0f5ba4ce162fe4ca85422
Ediversion <born4ice@hotmail.com>
2023-03-21 18:20:27 +0000

    thur

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This had better work
+ *
+ * Return: okay
+ */
+int main() {
+int sum = 0;
+for (int i = 1; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+printf("%d\n", sum);
+return (0);
