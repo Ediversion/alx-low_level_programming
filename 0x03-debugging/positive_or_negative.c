36b9dd5b2923953a4cb6ccc9ab68008503fc73ef
Ediversion <born4ice@hotmail.com>
2023-03-21 22:16:20 +0000

    zero

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *positive_or_negative - This had better work
+ *@i: num
+ * Return: okay
+ */
+
+void positive_or_negative(int i)
+{
+if (i < 0)
+	printf("%d is negative\n", i);
+else
+	printf("%d is positive\n", i);
+}
