7167c25d54ec26dcddef7f79e4af199a3eebf207
Ediversion <born4ice@hotmail.com>
2023-03-21 16:47:45 +0000

    seven

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - This had better work
+ *@i: p
+ * Return: okay
+ */
+int print_last_digit(i)
+{
+int k;
+k = i % 10;
+if (i < 10)
+k = -k;
+return (k);
+}
