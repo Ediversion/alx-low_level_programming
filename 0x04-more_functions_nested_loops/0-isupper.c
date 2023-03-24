77b0a6e8049b9045aa9652297bad890880793a81
Ediversion <born4ice@hotmail.com>
2023-03-24 04:38:49 +0000

    first

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+ * _isupper - This had better work
+ *@x: x
+ * Return: okay
+ */
+
 int _isupper(int x)
 {
-	if(x >= 65 && x <= 90)
+	if (x >= 65 && x <= 90)
 	{
 	return (1);
 	}
-	return(0);
+	return (0);
 }
