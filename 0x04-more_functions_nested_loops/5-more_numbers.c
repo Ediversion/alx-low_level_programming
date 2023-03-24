2712f1cf5fd225f154c6dae92162bad1a00096dd
Ediversion <born4ice@hotmail.com>
2023-03-24 05:42:53 +0000

    five

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - This had better work
+ *
+ * Return: okay
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+	for (y = 0; y <= 14; y++)
+	{
+	if (y > 9)
+	{
+	_putchar((y / 10) + '0');
+	}
+	_putchar((y % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
