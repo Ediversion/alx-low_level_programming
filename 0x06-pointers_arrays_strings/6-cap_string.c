b0acb406cb0a2b41bd68bff9598dd3002bcf29fc
Ediversion <born4ice@hotmail.com>
2023-03-30 06:45:11 +0000

    six

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_string - This had better work
+ * @s: s
+ * Return: okay
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+	while (!(s[i] >= 'a' && s[i] <= 'z'))
+	i++;
+	if (s[i - 1] == ' ' ||
+	s[i - 1] == '\t' ||
+	s[i - 1] == '\n' ||
+	s[i - 1] == ',' ||
+	s[i - 1] == ';' ||
+	s[i - 1] == '.' ||
+	s[i - 1] == '!' ||
+	s[i - 1] == '?' ||
+	s[i - 1] == '"' ||
+	s[i - 1] == '(' ||
+	s[i - 1] == ')' ||
+	s[i - 1] == '{' ||
+	s[i - 1] == '}' ||
+	i == 0)
+	s[i] -= 32;
+	i++;
+	}
+	return (s);
+}
