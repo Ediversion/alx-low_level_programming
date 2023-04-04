164cf15cd5c1bd5e74ada4fa871bc38ff607cd2f
Ediversion <born4ice@hotmail.com>
2023-04-04 00:07:25 +0000

    two

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strchr - This had better work
+ * @x: x
+ * @y: y
+ * Return: okay
+ */
+char *_strchr(char *x, char y)
+{
+	int i;
+
+	for (i = 0; x[i] >= '\0'; i++)
+	{
+	if (x[i] == y)
+	return (x +i);
+	}
+	return (NULL);
+}
