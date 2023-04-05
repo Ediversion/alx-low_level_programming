65292a6bac6064e2445d8f434ce46c2b24d44b13
Ediversion <born4ice@hotmail.com>
2023-04-05 20:46:01 +0000

    fove

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+int actual_sqrt_recursion(int a, int b);
+/**
+ * _sqrt_recursion - This had better work
+ * @n: n
+ * Return: okay
+ */
+int _sqrt_recursion(int x)
+{
+	if (x < 0)
+		return (-1);
+	return (actual_sqrt_recursion(x, 0));
+}
+/**
+ * actual_sqrt_recursion - This had better work
+ * @a: a
+ * @b: b
+ * Return: okay
+ */
+int actual_sqrt_recursion(int a, int b)
+{
+	if (b * b > a)
+		return (-1);
+	if (b * b == a)
+		return (b);
+	return (actual_sqrt_recursion(a, b + 1));
+}
