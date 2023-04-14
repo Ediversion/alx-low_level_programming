cadeb6f8d63b4e4975287879d0ce974af84a59ee
Ediversion <born4ice@hotmail.com>
2023-04-14 21:33:55 +0000

    two

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _calloc - cool
+* @nmemb: mrmb
+* @size: size
+* Return: okay
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *a;
+unsigned int b;
+if (nmemb == 0 || size == 0)
+return (NULL);
+a = malloc(nmemb * size);
+if (a == NULL)
+return (NULL);
+for (b = 0; b < (nmemb * size); b++)
+a[b] = 0;
+return (a);
+}
